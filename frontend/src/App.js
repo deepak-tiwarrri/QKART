022988f3ee1ae71d8318130d833b2f774b9d1a6c
Deepak483 <ainutiwari483@gmail.com>
2023-12-29 18:01:06 +0530

    backend link added  (Crio.Do)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,8 @@ import Thanks from "./components/Thanks";
 import ipConfig from "./ipConfig.json";
 
 export const config = {
-  endpoint: `http://${ipConfig.workspaceIp}:8082/v1`,
+  // endpoint: `http://${ipConfig.workspaceIp}:8082/v1`,
+  endpoint:"https://qkart-backend-first.onrender.com"
 };
 
 export default function App(props) {
